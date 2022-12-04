99905c1b2c054aea6c937f3303e19cd9d9285573
Elias2389 <andreselias2303@gmail.com>
2022-12-04 09:22:15 -0300

    setup new db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeormService } from './typeorm/typeorm.service';
